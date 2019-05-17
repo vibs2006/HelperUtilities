f5b82f2f83eb691d8464dfaeae0fdcb29069e5b9
Vaibhav Agarwal <vaibhav@parcelhero.in>
2019-05-17 14:58:59 +0530

    Added Json method to convert object into string.

---
+++ HelperUtilities/Json/JsonUtilities.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperUtilities.Json
+{
+    public static class JsonUtilities
+    {
+
+        public static string ConvertObjectToString(object obj, Formatting formatting = Formatting.None)
+        {
+            return JsonConvert.SerializeObject(obj, formatting);
+        }
+
+    }
+}
