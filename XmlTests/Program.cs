3d4e9b0a082dc10582e93f7b3d9c6e90ae0550b1
Vaibhav Agarwal <vaibhav@parcelhero.in>
2019-08-12 13:12:51 +0530

    Added Xml Parsing Facility

---
+++ XmlTests/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace XmlTests
+{
+    public static class util
+    {
+        public static T ParseXml<T>(this string value) where T : class
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var textReader = new StringReader(value))
+            {
+                return (T)xmlSerializer.Deserialize(textReader);
+            }
+        }
+
+        public static string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory() + @"\xmldata2.xml";
+            var xmlInputData = File.ReadAllText(path);
+
+            //Customer customer =util.ParseXml<Customer>(xmlInputData);
+           // object customer = util.ParseXml<object>(xmlInputData);
+            XDocument doc = XDocument.Parse(xmlInputData); //or XDocument.Load(path)
+
+            string jsonText = JsonConvert.SerializeXNode(doc);
+
+            dynamic dyn = JsonConvert.DeserializeObject<ExpandoObject>(jsonText);
+
+            Console.ReadLine();
+        }
+    }
+
+
+    
+    public class Product
+    {
+    
+        public string ProductID { get; set; }
+    
+        public string ProductName { get; set; }
+    
+        public string SupplierID { get; set; }
+    
+        public string CategoryID { get; set; }
+    
+        public string QuantityPerUnit { get; set; }
+    
+        public string UnitPrice { get; set; }
+        
+        public string UnitsInStock { get; set; }
+        
+        public string UnitsOnOrder { get; set; }
+        
+        public string ReorderLevel { get; set; }
+        
+        public string Discontinued { get; set; }
+    }
+
+    
+    public class Order_Detail
+    {
+    
+        public string OrderID { get; set; }
+    
+        public string ProductID { get; set; }
+        
+        public string UnitPrice { get; set; }
+        
+        public string Quantity { get; set; }
+        
+        public string Discount { get; set; }
+        
+        public Product Product { get; set; }
+    }
+
+    
+    public class Order_Details
+    {
+        public List<Order_Detail> Order_Detail { get; set; }
+    }
+
+    
+    public class Order
+    {
+    
+        public string OrderID { get; set; }
+    
+        public string CustomerID { get; set; }
+    
+        public string EmployeeID { get; set; }
+    
+        public string OrderDate { get; set; }
+    
+        public string RequiredDate { get; set; }
+        
+        public string ShippedDate { get; set; }
+        
+        public string ShipVia { get; set; }
+        
+        public string Freight { get; set; }
+        
+        public string ShipName { get; set; }
+        
+        public string ShipAddress { get; set; }
+        
+        public string ShipCity { get; set; }
+        
+        public string ShipPostalCode { get; set; }
+        
+        public string ShipCountry { get; set; }
+        
+        public Order_Details Order_Details { get; set; }
+    }
+
+    
+    public class Orders
+    {    
+        public List<Order> Order { get; set; }
+        public Orders()
+        {
+            Order = new List<Order>();
+        }
+    }
+    
+    public class Customer
+    {
+     
+        public string CustomerID { get; set; }
+     
+        public string CompanyName { get; set; }
+     
+        public string ContactName { get; set; }
+     
+        public string ContactTitle { get; set; }
+     
+        public string Address { get; set; }
+        
+        public string City { get; set; }
+        
+        public string PostalCode { get; set; }
+        
+        public string Country { get; set; }
+        
+        public string Phone { get; set; }
+        
+        public string Fax { get; set; }
+        
+        public Orders Orders { get; set; }
+    }
+}
---
+++ XmlTests/XmlConvert.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlTests
+{
+    public static class XmlConvert
+    {
+        public static string SerializeObject<T>(T dataObject)
+        {
+            if (dataObject == null)
+            {
+                return string.Empty;
+            }
+            try
+            {
+                using (StringWriter stringWriter = new System.IO.StringWriter())
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    serializer.Serialize(stringWriter, dataObject);
+                    return stringWriter.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static T DeserializeObject<T>(string xml)
+             where T : new()
+        {
+            if (string.IsNullOrEmpty(xml))
+            {
+                return new T();
+            }
+            try
+            {
+                using (var stringReader = new StringReader(xml))
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    return (T)serializer.Deserialize(stringReader);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new T();
+            }
+        }
+    }
+}
