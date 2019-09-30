d1d0eed7f35610d651652bbdea75c9de79374833
vibs2006 <vibs2006@gmail.com>
2019-09-30 11:25:42 +0530

    Added Remote IP Detection Methods
    Added Method for Remote IP Address Detection

--- HelperUtilities/Net/NetMethods.cs
+++ HelperUtilities/Net/NetMethods.cs
@@ -13,6 +13,40 @@ namespace HelperUtilities.Net
             return System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList.GetValue(1).ToString();
         }
 
+        public static string GetClientIpAddress(this HttpRequestMessage request)
+        {
+            // Web-hosting. Needs reference to System.Web.dll
+            if (request.Properties.ContainsKey(HttpContext))
+            {
+                dynamic ctx = request.Properties[HttpContext];
+                if (ctx != null)
+                {
+                    return ctx.Request.UserHostAddress;
+                }
+            }
+
+            // Self-hosting. Needs reference to System.ServiceModel.dll. 
+            if (request.Properties.ContainsKey(RemoteEndpointMessage))
+            {
+                dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+                if (remoteEndpoint != null)
+                {
+                    return remoteEndpoint.Address;
+                }
+            }
+
+            // Self-hosting using Owin. Needs reference to Microsoft.Owin.dll. 
+            if (request.Properties.ContainsKey(OwinContext))
+            {
+                dynamic owinContext = request.Properties[OwinContext];
+                if (owinContext != null)
+                {
+                    return owinContext.Request.RemoteIpAddress;
+                }
+            }
+
+            return string.Empty;
+        }
         //This Method Requires System.Web
         //public string GetIPAddress2()
         //{
