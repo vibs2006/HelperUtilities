24b117bd3174779ddc0b5e334685267d26dac81a
Vaibhav Agarwal <vaibhav@parcelhero.in>
2019-10-11 16:12:01 +0530

    Unit Tested Paging Methods on Lists

---
+++ HelperUtilities.Tests/Collections.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Collections.Generic;
+using HelperUtilities.Collections;
+
+namespace HelperUtilities.Tests
+{
+    [TestClass]
+    public class Collections
+    {
+        Random random = new Random();
+        public class testObj
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [TestMethod]
+        public void TestPagingList()
+        {            
+            List<testObj> objList = new List<testObj>();
+
+           
+            for (int i = 1; i <= 129; i++)
+            {
+                string s = string.Empty;
+                // random lowercase letter
+                for (int j = 1; j <= 10; j++)
+                {
+                    int a = random.Next(0, 26);
+                    char ch = (char)('a' + a);
+                    s = s + ch;
+                }
+                objList.Add(new testObj
+                {
+                    Id = i,
+                    Name = HelperUtilities.Text.StaticTextUtils.GenerateRandomWord()
+                });
+            }
+            Trace.WriteLine("List Generation is Completed for sample 23 Records");
+            foreach (var item in objList)
+            {
+                Trace.WriteLine(item.Id + " - " + item.Name);
+            }
+
+            int pageSize = 11;
+            int totalPages = objList.Count / pageSize;
+
+            Trace.WriteLine($"Total pages are {totalPages} with total count of {objList.Count} and pageSize of {pageSize}");
+            //var NewList = HelperUtilities.Collections.CollectionMethods.GetPage<testObj>(objList, totalPages, pageSize);
+            //Trace.WriteLine("Now Paging through last list");
+            //foreach (var item in NewList)
+            //{
+            //    Trace.WriteLine(item.Id + " -- " + item.Name);
+            //}
+
+            //NewList = CollectionMethods.GetPage(objList, 0, pageSize);
+            //Trace.WriteLine("Now Page through FIRST page of list");
+            //foreach (var item in NewList)
+            //{
+            //    Trace.WriteLine(item.Id + " -- " + item.Name);
+            //} 
+            Trace.WriteLine("Now Looping through all pages one by one");           
+            for (int i=0;i <= totalPages; i++)
+            {
+                Trace.WriteLine($"Page No. {i}\n starts");
+                var list = CollectionMethods.GetPage(objList, i, pageSize);
+                foreach(var item in list)
+                {
+                    Trace.WriteLine($"{item.Id} - {item.Name}");
+                }
+                Trace.WriteLine($"Page No. {i} ends");
+            }
+
+        }
+    }
+}
---
+++ HelperUtilities/Collections/CollectionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperUtilities.Collections
+{
+    public static class CollectionMethods
+    {
+        /// <summary>
+        /// It Returns the Current List on given Page. Please note that param currentPage have its starting value as 0 not 1.
+        /// </summary>
+        /// <typeparam name="T">Your Class</typeparam>
+        /// <param name="list"></param>
+        /// <param name="currentPage">currentPage have its starting value as 0 not 1. Last Page should be total list count divided by pageSize</param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static IList<T> GetPage<T>(IList<T> list, int currentPage, int pageSize)
+        {
+            return list.Skip(currentPage * pageSize).Take(pageSize).ToList();
+        }
+    }
+}
