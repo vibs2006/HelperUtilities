7546f4a46e3d2886d0adc4dd67083b7933ee90b1
vibs2006 <vibs2006@gmail.com>
2020-01-20 18:45:44 +0530

    Added Method to Take only N number of Left Chars from string irrespective of size

--- HelperUtilities/Text/TextUtils.cs
+++ HelperUtilities/Text/TextUtils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,24 +10,29 @@ namespace HelperUtilities.Text
     public static class StaticTextUtils
     {
         static Random random = new Random();
-        
-         public static string RemoveLineBreaks( this string lines )
-   {
-      return lines.Replace( "\r", "").Replace( "\n", "" );
-   }
 
-   public static string ReplaceLineBreaks( this string lines, string replacement )
-   {
-      return lines.Replace( "\r\n", replacement )
-                  .Replace( "\r", replacement )
-                  .Replace( "\n", replacement );
-   }
-        
+        public static string RemoveLineBreaks(this string lines)
+        {
+            return lines.Replace("\r", "").Replace("\n", "");
+        }
+
+        public static string Left(this string input, int maxLength)
+        {
+            return input.PadRight(maxLength).Substring(0, maxLength).TrimEnd();
+        }
+
+        public static string ReplaceLineBreaks(this string lines, string replacement)
+        {
+            return lines.Replace("\r\n", replacement)
+                        .Replace("\r", replacement)
+                        .Replace("\n", replacement);
+        }
+
         public static string GetNumbersFromString(string input)
         {
             return string.Join("", input.ToCharArray().Where(Char.IsDigit));
         }
-        
+
         public static string RemoveSpecialCharactersFromString(this string input)
         {
             return Regex.Replace(input.Trim(), @"[^0-9a-zA-Z]+", string.Empty);
@@ -35,8 +40,8 @@ namespace HelperUtilities.Text
 
         public static string GenerateRandomWord(int wordLength = 10, bool AppendNumberInEnd = true, int maxNumberValue = 100, bool firstCharaterAsUpperCase = true)
         {
-           
-            string s = string.Empty;            
+
+            string s = string.Empty;
             for (int j = 1; j <= wordLength; j++)
             {
                 char ch;
@@ -46,7 +51,7 @@ namespace HelperUtilities.Text
                     if (j == 1)
                     {
                         ch = Char.ToUpper((char)('a' + a));
-                        
+
                     }
                     else
                     {
