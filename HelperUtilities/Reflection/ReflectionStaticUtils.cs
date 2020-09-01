1f995d49e5e2c4fb93d40e8db5d70f9602e9b0f6
Vibs <vibs2006@gmail.com>
2020-09-01 19:16:19 +0530

    Added Method to Remove NULL properties from a class

---
+++ HelperUtilities/Reflection/ReflectionStaticUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperUtilities.Reflection
+{
+    public static class ReflectionStaticUtils
+    {
+        public static object ConvertToObjectWithoutPropertiesWithNullValues<T>(
+            this T objectToTransform
+            ,bool removeNulls = true)
+        {
+            var type = objectToTransform.GetType();
+            var returnClass = new ExpandoObject() as IDictionary<string, object>;
+            foreach (var propertyInfo in type.GetProperties())
+            {
+                var value = propertyInfo.GetValue(objectToTransform);
+                //var valueIsNotAString = !(value is string && !string.IsNullOrWhiteSpace(value.ToString()));
+                if (value != null && removeNulls == true)
+                {
+                    returnClass.Add(propertyInfo.Name, value);
+                }
+            }
+            return returnClass;
+        }
+    }
+}
