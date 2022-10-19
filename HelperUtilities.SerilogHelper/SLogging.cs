c2a990dabefcce99314fa376f068106d6eb9f465
Vaibhav Agarwal <vibs2006@gmail.com>
2022-10-19 19:50:57 +0530

    Serilog helper

---
+++ HelperUtilities.SerilogHelper/SLogging.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using System;
+using System.IO;
+namespace HelperUtilities.SerilogHelper
+{
+    public static class SLogging
+    {
+        public static ILogger GetSerilogTextWriter(string absoluteFolderPath = null)
+        {
+            string filePath;
+            if (string.IsNullOrEmpty(absoluteFolderPath) == false)
+            {
+                filePath = Path.Combine(absoluteFolderPath, "log.txt");
+            }
+            else
+            {
+                filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", "log.txt");
+            }
+
+            return new LoggerConfiguration().WriteTo.File(filePath, rollingInterval: RollingInterval.Day).CreateLogger();
+        }
+    }
+}
