d74c499999b105626fd801190aa20f5868f7a82a
Vaibhav Agarwal <vibs2006@gmail.com>
2022-11-08 14:33:07 +0530

    Ordinal suffix of a number

--- HelperUtilities/Text/TextUtils.cs
+++ HelperUtilities/Text/TextUtils.cs
@@ -156,5 +156,16 @@ namespace HelperUtilities.Text
             return _sb.ToString();
         }
 
+        private static string GetOrdinalSuffix(int num)
+        {
+            string number = num.ToString();
+            if (number.EndsWith("11")) return "th";
+            if (number.EndsWith("12")) return "th";
+            if (number.EndsWith("13")) return "th";
+            if (number.EndsWith("1")) return "st";
+            if (number.EndsWith("2")) return "nd";
+            if (number.EndsWith("3")) return "rd";
+            return "th";
+        }
     }
 }
